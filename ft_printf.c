a48401ba06bd9f46c5c9ab900e0e39f47d29230f
Nicolaas Van Straten <nvan-str@f1r5s17.codam.nl>
2022-11-11 17:30:27 +0100

    m

--- ft_printf.c
+++ ft_printf.c
@@ -1,38 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_putnbr.c                                        :+:    :+:            */
+/*   ft_printf.c                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: nvan-str <nvan-str@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/19 12:16:59 by nvan-str      #+#    #+#                 */
-/*   Updated: 2022/11/11 17:23:36 by nvan-str      ########   odam.nl         */
+/*   Created: 2022/11/11 17:20:04 by nvan-str      #+#    #+#                 */
+/*   Updated: 2022/11/11 17:21:13 by nvan-str      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
+#include "libftprintf.h"
 
-int	ft_putnbr(int n)
+int	ft_printf(const char *, ...)
 {
-	int	i;
-	int	printlen;
-
-	printlen = 0;
-	if (n == -2147483648)
-	{
-		ft_putstr_fd("-2147483648", 1);
-		return (12);
-	}
-	if (n < 0)
-	{
-		write (1, "-", 1);
-		n = -n;
-		printlen++;
-	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	i = n % 10 + 48;
-	write (1, &i, 1);
-	printlen++;
-	return (printlen);
+	
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,38 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_putnbr.c                                        :+:    :+:            */
+/*   libftprintf.h                                      :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: nvan-str <nvan-str@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/19 12:16:59 by nvan-str      #+#    #+#                 */
-/*   Updated: 2022/11/11 17:23:36 by nvan-str      ########   odam.nl         */
+/*   Created: 2022/11/11 16:58:18 by nvan-str      #+#    #+#                 */
+/*   Updated: 2022/11/11 16:59:56 by nvan-str      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
-int	ft_putnbr(int n)
-{
-	int	i;
-	int	printlen;
+# include <unistd.h>
 
-	printlen = 0;
-	if (n == -2147483648)
-	{
-		ft_putstr_fd("-2147483648", 1);
-		return (12);
-	}
-	if (n < 0)
-	{
-		write (1, "-", 1);
-		n = -n;
-		printlen++;
-	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	i = n % 10 + 48;
-	write (1, &i, 1);
-	printlen++;
-	return (printlen);
-}
+int	ft_putchar(char c);
+int	ft_putnbr(int n);
+int	ft_putstr(char *str);
+
+#endif 
